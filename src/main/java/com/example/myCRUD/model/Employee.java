1faf1b293ea2fb32882995ced62526b9ecfe31f5
Chriford Siame <chrifordsiame@booknowzambia.com>
2024-04-18 19:40:19 +0200

    relocate packages

---
+++ src/main/java/com/example/myCRUD/model/Employee.java
@@ -0,0 +1,51 @@
+package com.example.myCRUD.model;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Employee")
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Setter
+// @Getter
+// @ToString
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "salary")
+    private float salary;
+
+    public Employee() {}
+
+    public Employee(String name, float salary) {
+        super();
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
