503af6e796a36c59616dcf8f827f27c57f2ee1dc
Chriford Siame <chrifordsiame@booknowzambia.com>
2024-04-22 15:31:09 +0200

    Delete comments

--- src/main/java/com/example/myCRUD/MyCrudApplication.java
+++ src/main/java/com/example/myCRUD/MyCrudApplication.java
@@ -1,15 +1,27 @@
 package com.example.myCRUD;
+import java.util.List;
+
+import org.apache.el.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan;
 
 import com.example.myCRUD.controller.*;
 import com.example.myCRUD.model.*;
+import com.example.myCRUD.repo.UserRepo;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class MyCrudApplication {
 
+	// @Autowired
+	// public UserRepo userRepo;
+
+	// @PostConstruct
+	// public void initUsers() {
+
+	// }
 	public static void main(String[] args) {
 		SpringApplication.run(MyCrudApplication.class, args);
 	}
