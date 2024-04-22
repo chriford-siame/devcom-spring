eddceb270682a349faa1a69c97559303b923f1ad
Chriford Siame <chrifordsiame@booknowzambia.com>
2024-04-22 15:32:56 +0200

    sort imports

--- src/main/java/com/example/myCRUD/repo/EmployeeRepo.java
+++ src/main/java/com/example/myCRUD/repo/EmployeeRepo.java
@@ -2,7 +2,6 @@ package com.example.myCRUD.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.myCRUD.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 }
