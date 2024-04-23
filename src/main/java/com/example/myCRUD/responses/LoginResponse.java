b95db3f71d4e59172b90aac6d737a2facd2e5215
Chriford Siame <chrifordsiame@booknowzambia.com>
2024-04-23 16:55:57 +0200

    Add a login response

---
+++ src/main/java/com/example/myCRUD/responses/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.example.myCRUD.responses;
+
+public class LoginResponse {
+    private String token;
+
+    private long expiresIn;
+
+    public String getToken() {
+        return token;
+    }
+}
